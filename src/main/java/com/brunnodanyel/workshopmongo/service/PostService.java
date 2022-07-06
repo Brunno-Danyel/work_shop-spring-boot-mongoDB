4f66c16f0a9097cace558c800300b328eaf8843d
Brunno Danyel <brunnodanyel25rbr@hotmail.com>
2022-07-06 20:56:14 -0300

    Obtendo um post por Id

---
+++ src/main/java/com/brunnodanyel/workshopmongo/service/PostService.java
@@ -0,0 +1,28 @@
+package com.brunnodanyel.workshopmongo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.brunnodanyel.workshopmongo.domain.Post;
+import com.brunnodanyel.workshopmongo.repository.PostRepository;
+import com.brunnodanyel.workshopmongo.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	@Autowired
+	private PostRepository repo;
+
+	public List<Post> findAll() {
+		return repo.findAll();
+	}
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+
+}
